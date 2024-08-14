64346a3f214ba675ee3873391bd70810fcd62512
Mwangilwa Zimba <mwangilwa23@gmail.com>
2024-08-14 16:58:43 +0200

    done

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,8 +1,8 @@
-/// <reference path="subjects/Teacher.ts" />
-/// <reference path="subjects/Subject.ts" />
-/// <reference path="subjects/Cpp.ts" />
-/// <reference path="subjects/React.ts" />
-/// <reference path="subjects/Java.ts" />
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+/// <reference path="./Cpp.ts" />
+/// <reference path="./React.ts" />
+/// <reference path="./Java.ts" />
 
 export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
